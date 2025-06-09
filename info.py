abcc91f6dc42e3407975ef29ba13ddf079a7809a
Surajsurya95096 <surajroxofficial01@gmail.com>
2025-06-09 10:16:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 import os
 # Bot information
-API_ID = os.getenv('API_ID', "")
-API_HASH = os.getenv('API_HASH', "")
-BOT_TOKEN = os.getenv('BOT_TOKEN', "")
+API_ID = os.getenv('API_ID', "26992030")
+API_HASH = os.getenv('API_HASH', "4da7d71c6bc4512a886e41aca83a5ee3")
+BOT_TOKEN = os.getenv('BOT_TOKEN', "7861489855:AAHsF6S5n_3y7VO7RMwXL62oG2e_d_Qvwsc")
 
 # stream vars
 PORT = int(os.getenv('PORT', '5050'))
-BIN_CHANNEL = os.getenv("BIN_CHANNEL", "") #Log Channel
+BIN_CHANNEL = os.getenv("BIN_CHANNEL", "-1002494241960") #Log Channel
 URL = os.getenv("URL", "") #App URL not MongoDB URL
