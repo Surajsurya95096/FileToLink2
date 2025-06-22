2e9909940003db4610db353094eba4556645bfb9
Surajsurya95096 <surajroxofficial01@gmail.com>
2025-06-22 22:24:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,9 +2,9 @@ import os
 # Bot information
 API_ID = os.getenv('API_ID', "26992030")
 API_HASH = os.getenv('API_HASH', "4da7d71c6bc4512a886e41aca83a5ee3")
-BOT_TOKEN = os.getenv('BOT_TOKEN', "7861489855:AAHsF6S5n_3y7VO7RMwXL62oG2e_d_Qvwsc")
+BOT_TOKEN = os.getenv('BOT_TOKEN', "")
 
 # stream vars
 PORT = int(os.getenv('PORT', '5050'))
-BIN_CHANNEL = os.getenv("BIN_CHANNEL", "-1002494241960") #Log Channel
+BIN_CHANNEL = os.getenv("BIN_CHANNEL", "-1002402091090") #Log Channel
 URL = os.getenv("URL", "https://filetolink2-1.onrender.com") #App URL not MongoDB URL
